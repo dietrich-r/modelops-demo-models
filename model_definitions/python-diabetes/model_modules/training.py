d4c9681eeaf9a44623cc7ec6813147a48da46179
dietrich-r <148858046+dietrich-r@users.noreply.github.com>
2023-10-24 15:29:57 +0200

    Update training.py according to instruction in "07_ModelOps_GIT_PIMA_Python_Indb_GLM"

--- model_definitions/python-diabetes/model_modules/training.py
+++ model_definitions/python-diabetes/model_modules/training.py
@@ -1,16 +1,26 @@
-from xgboost import XGBClassifier
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import Pipeline
-from nyoka import xgboost_to_pmml
-from teradataml import DataFrame
+from teradataml import (
+    DataFrame,
+    GLM,
+    ScaleFit,
+    ScaleTransform
+)
 from aoa import (
     record_training_stats,
     save_plot,
     aoa_create_context,
     ModelContext
 )
+import numpy as np
 
-import joblib
+
+def plot_feature_importance(fi, img_filename):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    feat_importances = pd.Series(fi)
+    feat_importances.nlargest(10).plot(kind='barh').set_title('Feature Importance')
+    fig = plt.gcf()
+    fig.savefig(img_filename, dpi=500)
+    plt.clf()
 
 
 def train(context: ModelContext, **kwargs):
@@ -18,45 +28,71 @@ def train(context: ModelContext, **kwargs):
 
     feature_names = context.dataset_info.feature_names
     target_name = context.dataset_info.target_names[0]
+    entity_key = context.dataset_info.entity_key
 
     # read training dataset from Teradata and convert to pandas
     train_df = DataFrame.from_query(context.dataset_info.sql)
-    train_pdf = train_df.to_pandas(all_rows=True)
 
-    # split data into X and y
-    X_train = train_pdf[feature_names]
-    y_train = train_pdf[target_name]
+    print ("Scaling using InDB Functions...")
+    
+    scaler = ScaleFit(
+        data=train_df,
+        target_columns = feature_names,
+        scale_method = context.hyperparams["scale_method"],
+        miss_value = context.hyperparams["miss_value"],
+        global_scale = context.hyperparams["global_scale"].lower() in ['true', '1'],
+        multiplier = context.hyperparams["multiplier"],
+        intercept = context.hyperparams["intercept"]
+    )
 
+    scaled_train = ScaleTransform(
+        data=train_df,
+        object=scaler.output,
+        accumulate = [target_name,entity_key]
+    )
+    
+    scaler.output.to_sql(f"scaler_${context.model_version}", if_exists="replace")
+    print("Saved scaler")
+    
     print("Starting training...")
 
-    # fit model to training data
-    model = Pipeline([('scaler', MinMaxScaler()),
-                      ('xgb', XGBClassifier(eta=context.hyperparams["eta"],
-                                            max_depth=context.hyperparams["max_depth"]))])
-
-    model.fit(X_train, y_train)
-
-    print("Finished training")
-
-    # export model artefacts
-    joblib.dump(model, f"{context.artifact_output_path}/model.joblib")
-
-    # we can also save as pmml so it can be used for In-Vantage scoring etc.
-    xgboost_to_pmml(pipeline=model, col_names=feature_names, target_name=target_name,
-                    pmml_f_name=f"{context.artifact_output_path}/model.pmml")
-
+    model = GLM(
+        input_columns = feature_names,
+        response_column = target_name,
+        data = scaled_train.result,
+        family = context.hyperparams["family"],
+        learning_rate = context.hyperparams["learning_rate"],
+        momentum = context.hyperparams["momentum"],
+        initial_eta = context.hyperparams["initial_eta"],
+        local_sgd_iterations = context.hyperparams["local_sgd_iterations"],
+        iter_max = context.hyperparams["iter_max"],
+        batch_size = context.hyperparams["batch_size"],
+        iter_num_no_change = context.hyperparams["iter_num_no_change"]
+    )
+    
+    model.result.to_sql(f"model_${context.model_version}", if_exists="replace")    
     print("Saved trained model")
 
-    from xgboost import plot_importance
-    model["xgb"].get_booster().feature_names = feature_names
-    plot_importance(model["xgb"].get_booster(), max_num_features=10)
-    save_plot("feature_importance.png", context=context)
+    # Calculate feature importance and generate plot
+    model_pdf = model.result.to_pandas()[['predictor','estimate']]
+    predictor_dict = {}
+    
+    for index, row in model_pdf.iterrows():
+        if row['predictor'] in feature_names:
+            value = row['estimate']
+            predictor_dict[row['predictor']] = value
+    
+    feature_importance = dict(sorted(predictor_dict.items(), key=lambda x: x[1], reverse=True))
+    keys, values = zip(*feature_importance.items())
+    norm_values = (values-np.min(values))/(np.max(values)-np.min(values))
+    feature_importance = {keys[i]: float(norm_values[i]*1000) for i in range(len(keys))}
+    plot_feature_importance(feature_importance, f"{context.artifact_output_path}/feature_importance")
 
-    feature_importance = model["xgb"].get_booster().get_score(importance_type="weight")
-
-    record_training_stats(train_df,
-                          features=feature_names,
-                          targets=[target_name],
-                          categorical=[target_name],
-                          feature_importance=feature_importance,
-                          context=context)
+    record_training_stats(
+        train_df,
+        features=feature_names,
+        targets=[target_name],
+        categorical=[target_name],
+        feature_importance=feature_importance,
+        context=context
+    )
